1cc5c43b06447fc6b6ecb6ae852ddfc1e649e38a
micha <mbladowski@macropage.de>
2023-06-29 01:22:37 +0200

    add missing docblocks

--- src/Facades/Daparto.php
+++ src/Facades/Daparto.php
@@ -5,8 +5,15 @@ namespace macropage\laravel_daparto\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class DapartoF
+ * Class Daparto
  * @package macropage\laravel_daparto
+ * @method static void setCustomerConfig(string $customer)
+ * @method static array getDistinctShippingDescr(array $orders)
+ * @method static array getCustomerConfig()
+ * @method static bool  setDone(string $file)
+ * @method static array getSingleXMLOrder(string $orderId, bool $useCache = false)
+ * @method static bool|string uploadShippingData(string $order_number, string $carrier, string $shipping_number)
+ * @method static array getXMLOrders(bool $useCache = false)
  */
 class Daparto extends Facade {
     /**
@@ -15,6 +22,6 @@ class Daparto extends Facade {
      * @return string
      */
     protected static function getFacadeAccessor(): string {
-        return 'daparto';
+        return daparto::class;
     }
 }
